326ef2e7ee3996a38d4cf71663a37b54263d83bf
mahedes <mahevadessart@gmail.com>
2022-11-11 18:11:47 +0100

    08_02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  secondes: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    const compteur = interval(1000);
 
+    compteur.subscribe({
+      next: (v) => this.secondes = v,
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    })
   }
 
 }
