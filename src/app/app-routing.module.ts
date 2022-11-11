259e5cc11499ac7b74c3bdeaedc3a6f2bb237abf
mahedes <mahevadessart@gmail.com>
2022-11-11 17:24:33 +0100

    06_07

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailsComponent } from './details/details.component';
 import { ListeComponent } from './liste/liste.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: "", component: ListeComponent },
   { path: "liste", redirectTo: "" },
-  { path: "details/:id", component: DetailsComponent }
+  { path: "details/:id", component: DetailsComponent },
+  { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FormsModule } from '@angular/forms';
 import { DataService } from './data.service';
 import { ListeComponent } from './liste/liste.component';
 import { DetailsComponent } from './details/details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
     ListeComponent,
-    DetailsComponent
+    DetailsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
