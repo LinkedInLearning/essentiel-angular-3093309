d6bb786098df83403e6e1e472c8e77d74a4e54f4
mahedes <mahevadessart@gmail.com>
2022-11-11 17:49:34 +0100

    07_02_d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AjouterComponent } from './ajouter/ajouter.component';
 import { DetailsComponent } from './details/details.component';
 import { ListeComponent } from './liste/liste.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: "", component: ListeComponent },
   { path: "liste", redirectTo: "" },
   { path: "details/:id", component: DetailsComponent },
+  { path: "ajouter", component: AjouterComponent },
   { path: "**", component: PageNotFoundComponent }
 ];
 
