bba15ee610bc94778c398fdead9b761dfdbb4283
mahedes <mahevadessart@gmail.com>
2022-11-11 16:09:36 +0100

    05_03

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   message: string = "";
+  liste;
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.liste = this.dataService.listeArticles;
+  }
 
   onAffiche(arg: string) {
     return this.message = "Merci d'avoir voté pour l'article : " + arg;
   }
 
-  liste = [
-    {
-      titreArticle: "Vélo",
-      prixArticle: 80,
-      description: "Un super vélo tout terrain",
-      urlImg: "../assets/images/velo.jpeg",
-      textAltImg: "Un vélo",
-      dispo: false
-    },
-    {
-      titreArticle: "TV",
-      prixArticle: 230,
-      description: "Très bonne qualité d'écran",
-      urlImg: "../assets/images/tv.jpeg",
-      textAltImg: "Une télévision",
-      dispo: true
-    },
-    {
-      titreArticle: "Jouet",
-      prixArticle: 15,
-      description: "Neuf, jamais utilisé",
-      urlImg: "../assets/images/jouet.jpeg",
-      textAltImg: "Un jouet pour enfant",
-      dispo: true
-    }
-  ]
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,6 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
+  listeArticles = [
+    {
+      titreArticle: "Vélo",
+      prixArticle: 80,
+      description: "Un super vélo tout terrain",
+      urlImg: "../assets/images/velo.jpeg",
+      textAltImg: "Un vélo",
+      dispo: false
+    },
+    {
+      titreArticle: "TV",
+      prixArticle: 230,
+      description: "Très bonne qualité d'écran",
+      urlImg: "../assets/images/tv.jpeg",
+      textAltImg: "Une télévision",
+      dispo: true
+    },
+    {
+      titreArticle: "Jouet",
+      prixArticle: 15,
+      description: "Neuf, jamais utilisé",
+      urlImg: "../assets/images/jouet.jpeg",
+      textAltImg: "Un jouet pour enfant",
+      dispo: true
+    }
+  ]
 
   constructor() { }
 }
